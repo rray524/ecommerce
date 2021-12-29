74d832891fa0552c7fe3fc74f5ccb788637716d8
rray524 <rray524@gmail.com>
2021-12-29 08:42:00 +0600

    register-productslist & single product page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Home from './Pages/Home';
+import ProductSingle from './Pages/ProductSingle';
+import Register from './Pages/Register';
+
 
 function App() {
   return (
     <div className="main">
-      <Home />
+      <Register />
     </div>
   );
 }
